5a6b5f4a0ec6b63940d31c9df664f990179ea1c3
MaxRev <cmthea32@gmail.com>
2021-02-11 06:28:11 +0200

    year fixup

--- calc/src/Services/Calculator.cs
+++ calc/src/Services/Calculator.cs
@@ -65,7 +65,7 @@ namespace NUWM.Servers.Core.Calc.Services
             double? prepCourses,
             int? ukrOlimp)
         {
-            var year = _config.CurrentYear;
+            var year = _config.CurrentYear - 1;
             var obj = new List<CalculatedSpecialty>();
             IEnumerable<SpecialtyInfo> listing;
 
